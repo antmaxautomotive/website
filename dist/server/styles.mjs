7ff07374d3c56a8eab231c21462f81603d33b844
Qbiak <52896589+Qbiak-code@users.noreply.github.com>
2025-05-05 23:37:14 +0100

    Updates

---
+++ dist/server/styles.mjs
@@ -0,0 +1,6 @@
+const interopDefault = r => r.default || r || []
+export default {
+  "node_modules/nuxt/dist/app/entry.js": () => import('./.nuxt/entry-styles.de0f79af.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": () => import('./.nuxt/error-500-styles.7d49cc85.mjs').then(interopDefault),
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": () => import('./.nuxt/error-404-styles.907be083.mjs').then(interopDefault)
+}
--- server/chunks/rollup/_virtual_head-static.mjs
+++
@@ -1,4 +0,0 @@
-const _virtual__headStatic = {"headTags":"<meta charset=\"utf-8\">\n<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<meta name=\"og:image\" content=\"@/assets/logo512.png\">\n<link title=\"favicon\" rel=\"icon\" type=\"image/png\" href=\"@/assets/logo.png\">\n<link title=\"mobileicon\" rel=\"apple-touch-icon\" sizes=\"512x512\" href=\"@/assets/logo.png\">","bodyTags":"","bodyTagsOpen":"","htmlAttrs":"","bodyAttrs":""};
-
-export { _virtual__headStatic as default };
-//# sourceMappingURL=_virtual_head-static.mjs.map
